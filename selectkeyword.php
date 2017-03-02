b0fb45eaf2f43e0be3f7dcad8c4aa19a37f77607
honeyfunnybunny <1530417351@qq.com>
2017-03-03 00:37:48 +0800

    Update selectkeyword.php

--- selectkeyword.php
+++ selectkeyword.php
@@ -3,23 +3,24 @@ $service='mysql:host=localhost;dbname=segmentfault';
 $name='root';
 $code='';
 $keyword=$_POST['keyword'];
-try{
+print_r($_POST);
+try
+{
 	$pdo = new PDO($service , 'root' , '');
 	$pdo -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $result = $pdo -> query("select * from selectkeyword where keyword='{$keyword}'");
+    $result = $pdo -> query("select * from selectkeyword																																								 where keyword='{$keyword}'");
     $data = $result -> fetch(PDO::FETCH_ASSOC);
     if ($keyword == $data['keyword']) 
-        {
-            $url="";
-            echo "<script>window.location.href='$url';</script>";
-    	}
+    {
+        echo "关键词已存在";
+    }
     else 
-        {
-            $url="";
-            echo "<script>window.location.href='$url';</script>";
-    	}
+    {
+        echo "关键词不存在";
+    }
 }
-catch (PDOException $e) {
-            echo "数据库连接失败";
+    catch (PDOException $e) 
+    {
+        echo "数据库连接失败";
     }
 ?>
